61262c12c7f6623e53b71b23073dc041121d2226
Steven Clontz <steven.clontz@gmail.com>
2024-06-17 19:15:18 +0000

    toy pelican implementation

---
+++ site/content/pages/home.md
@@ -0,0 +1,12 @@
+---
+title: My super title
+date: 2010-12-03 10:20
+modified: 2010-12-05 19:30
+category: Python
+tags: pelican, publishing
+slug: my-super-post
+authors: Alexis Metaireau, Conan Doyle
+summary: Short version for index and feeds
+---
+
+Homepage info...
---
+++ site/pelicanconf.py
@@ -0,0 +1,22 @@
+AUTHOR = 'AUTHOR_FROM_SOURCE'
+SITENAME = 'TITLE_FROM_SOURCE'
+SITEURL = "BASE_URL_FROM_SOURCE"
+
+PATH = "content"
+THEME = "theme"
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
