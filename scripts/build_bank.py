129eb16ea6df6ce4cc93cb71493a1e145c42a620
Steven Clontz <steven.clontz@gmail.com>
2024-07-22 21:32:25 -0500

    add checkit (#7)
    * modify quick/long build in CI
    
    * fix (?) build all conditional
    
    * fix (?!) build all conditional
    
    * update library page
    
    * update pretext
    
    * tweak about page
    
    * modify about page
    
    * more about page tweaks
    
    * edit url in about page
    
    * add bank builds to workflow
    
    * add full bank build support
    
    * rename build_bank
    
    * add outcome preview feature
    
    * delete checkit builds before cloudflare deployment
    
    * smarter file deletion

---
+++ scripts/build_bank.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import shutil
+import zipfile
+from checkit.bank import Bank
+from checkit import static
+
+parser = argparse.ArgumentParser(description='Build a CheckIt bank.')
+parser.add_argument('book', choices=['precalculus', 'calculus', 'linear-algebra'])
+parser.add_argument('-f', '--full',
+                    action='store_true')
+args = parser.parse_args()
+
+bank_path = os.path.join(args.book,"exercises")
+
+print(f"Building exercises from `{bank_path}`")
+
+b = Bank(bank_path)
+b.generate_exercises(regenerate=args.full, images=args.full)
+b.write_json()
+
+# b.build_viewer() # <-- broken, reimplemented below
+docs_path = os.path.join(b.abspath(), "docs")
+if os.path.exists(docs_path) and os.path.isdir(docs_path):
+    shutil.rmtree(docs_path)
+os.makedirs(docs_path)
+archive = zipfile.ZipFile(static.open_resource("viewer.zip"))
+archive.extractall(docs_path)
+# copy assets
+shutil.copytree(b.build_path(), os.path.join(docs_path,"assets"), dirs_exist_ok=True)
+
+stage_path = os.path.join("output", "stage", args.book, "preview", "exercises")
+print(f"Staging bank at `{stage_path}`")
+# stage bank
+shutil.copytree(
+    docs_path, 
+    stage_path, 
+    dirs_exist_ok=True
+)
---
+++ scripts/preview_outcome.py
@@ -0,0 +1,78 @@
+import argparse
+import shutil
+from pathlib import Path
+import os
+import subprocess
+import zipfile
+import lxml.etree as ET
+from checkit.bank import Bank
+from checkit import static
+
+parser = argparse.ArgumentParser(description='Preview a CheckIt outcome.')
+parser.add_argument('book', choices=['precalculus', 'calculus', 'linear-algebra'])
+parser.add_argument('outcome')
+args = parser.parse_args()
+
+# check that this outcome exists
+exercise_path = Path(args.book, "exercises")
+bank_root = ET.parse(exercise_path/"bank.xml")
+search_string = f'.//*[name()="slug" and text()="{args.outcome}"]/./..'
+outcome_ele = bank_root.xpath(search_string)[0]
+
+# get path to sandbox
+sandbox_path = Path("exercise-sandbox")
+
+# destroy any existing sandbox
+shutil.rmtree(sandbox_path, ignore_errors=True)
+
+# create new sandbox with that outcome
+sandbox_path.mkdir()
+(sandbox_path/"bank.xml").write_text(f"""
+<?xml version='1.0' encoding='UTF-8'?>
+<bank xmlns="https://checkit.clontz.org" version="0.2">
+    <title>Preview: {args.book} {args.outcome}</title>
+    <slug>preview</slug>
+    <url>https://tbil.org</url>
+    <outcomes>
+        <outcome>
+            <title>{outcome_ele.find("{*}title").text}</title>
+            <slug>{args.outcome}</slug>
+            <path>outcome</path>
+            <description>{outcome_ele.find("{*}description").text}</description>
+        </outcome>
+    </outcomes>
+</bank>
+""".strip())
+(sandbox_path/"outcome").mkdir()
+(sandbox_path/"library").mkdir()
+if args.book == "linear-algebra":
+    shutil.copy(exercise_path/"library"/"common.sage", sandbox_path/"library"/"common.sage")
+outcome_path = (exercise_path/outcome_ele.find("{*}path").text)
+shutil.copy(outcome_path/"template.xml", sandbox_path/"outcome"/"template.xml")
+shutil.copy(outcome_path/"generator.sage", sandbox_path/"outcome"/"generator.sage")
+
+b = Bank(str(sandbox_path))
+print('generate exercises')
+
+# build 20 seeds and images
+cmds = [
+    "sage",
+    os.path.join("scripts", "preview_outcome.sage"),
+    args.outcome
+]
+subprocess.run(cmds,check=True)
+
+b.write_json()
+
+# b.build_viewer() # <-- broken, reimplemented below
+docs_path = os.path.join(b.abspath(), "docs")
+if os.path.exists(docs_path) and os.path.isdir(docs_path):
+    shutil.rmtree(docs_path)
+os.makedirs(docs_path)
+archive = zipfile.ZipFile(static.open_resource("viewer.zip"))
+archive.extractall(docs_path)
+# copy assets
+shutil.copytree(b.build_path(), os.path.join(docs_path, "assets"), dirs_exist_ok=True)
+
+
+subprocess.run(['python', '-m', 'http.server', '-d', str(sandbox_path/'docs')])
