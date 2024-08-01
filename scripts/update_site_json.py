99e5114fdeabd4a2db1f51398331211f0c06e767
Steven Clontz <steven.clontz@gmail.com>
2024-08-01 03:20:40 +0000

    update ptx to support site.json

---
+++ scripts/update_site_json.py
@@ -0,0 +1,16 @@
+import json
+
+def main():
+    site_dict = {
+        "SITENAME": "Team-Based Inquiry Learning",
+        "SITESUBTITLE": "Open Resources and Research supporting Active Learning in Mathematics",
+        "PTX_SHOW_TARGETS": "no",
+        "STATIC_PATHS": ["static", "precalculus", "calculus", "linear-algebra"]
+    }
+    with open("site/description.html") as f:
+        site_dict["PTX_SITE_DESCRIPTION"] = f.read()
+    with open("site/site.json", "w") as f:
+        json.dump(site_dict, f, indent=4)
+
+if __name__ == "__main__":
+    main()
