c3fd465ed8203a7c5572798815aeea599cec0a2d
Steven Clontz <steven.clontz@gmail.com>
2024-08-01 00:22:33 +0000

    add scripts

---
+++ scripts/build_edition.py
@@ -0,0 +1,57 @@
+import argparse
+import glob
+import build_bank
+from pretext.project import Project
+
+def main():
+    parser = argparse.ArgumentParser(description='Build fixed editions of books.')
+    parser.add_argument('edition_name')
+    args = parser.parse_args()
+ 
+    # update edition name
+    files = glob.glob('source/**/source/main.ptx')
+    files += glob.glob('source/**/exercises/bank.xml')
+    for filestr in files:
+        # Read in the file
+        with open(filestr, 'r') as file:
+            filedata = file.read()
+        # Replace the target string
+        filedata = filedata.replace(
+            "<!-- edition -->PREVIEW<!-- /edition -->", 
+            f"<!-- edition -->{args.edition_name}<!-- /edition -->"
+        )
+        # Write the file out again
+        with open(filestr, 'w') as file:
+            file.write(filedata)
+
+    # update edition links
+    files = glob.glob('source/**/source/**/*.ptx')
+    for filestr in files:
+        # Read in the file
+        with open(filestr, 'r') as file:
+            filedata = file.read()
+        # Replace the target string
+        filedata = filedata.replace(
+            "https://tbil.org/calculus/preview", 
+            f"https://tbil.org/calculus/{args.edition_name}"
+        )
+        filedata = filedata.replace(
+            "https://tbil.org/precalculus/preview", 
+            f"https://tbil.org/precalculus/{args.edition_name}"
+        )
+        filedata = filedata.replace(
+            "https://tbil.org/linear-algebra/preview", 
+            f"https://tbil.org/linear-algebra/{args.edition_name}"
+        )
+        # Write the file out again
+        with open(filestr, 'w') as file:
+            file.write(filedata)
+
+    # p = Project.parse()
+    # for t in p.deploy_targets():
+    #     t.build()
+
+    # build_bank.main()
+
+if __name__ == "__main__":
+    main()
