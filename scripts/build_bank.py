0e9969024d52489c0729e1b3122dbadf4d0eb040
Steven Clontz <steven.clontz@gmail.com>
2024-08-02 15:15:36 -0500

    Add support for releasing editions (#209)
    * add scripts
    
    * update la exercise links
    
    * edition name and slug, don't count on comments
    
    * work on scripts
    
    * update ptx to support site.json
    
    * rename publish_edition
    
    * git management
    
    * remove future ON section
    
    * fix alignat columns
    
    * fix ol markers
    
    * fix git subprocess
    
    * remove non-functioning git reset
    
    * build edition in ignored subdirectory
    
    * clean up preview outcome script to be importable
    
    * update precal frontmatter
    
    * add python module file
    
    * more scripting
    
    * Update source/precalculus/source/meta/frontmatter.ptx
    
    * Update source/precalculus/source/meta/frontmatter.ptx
    
    Co-authored-by: Drew Lewis <30658947+siwelwerd@users.noreply.github.com>
    
    * Update source/precalculus/source/meta/frontmatter.ptx
    
    Co-authored-by: Drew Lewis <30658947+siwelwerd@users.noreply.github.com>
    
    * build precal bank, with caveat in text
    
    ---------
    
    Co-authored-by: Drew Lewis <30658947+siwelwerd@users.noreply.github.com>

--- scripts/build_bank.py
+++ scripts/build_bank.py
@@ -5,35 +5,39 @@ import zipfile
 from checkit.bank import Bank
 from checkit import static
 
-parser = argparse.ArgumentParser(description='Build a CheckIt bank.')
-parser.add_argument('book', choices=['precalculus', 'calculus', 'linear-algebra'])
-parser.add_argument('-f', '--full',
-                    action='store_true')
-args = parser.parse_args()
+def main(book:str, full:bool):
 
-bank_path = os.path.join("source", args.book, "exercises")
+    bank_path = os.path.join("source", book, "exercises")
 
-print(f"Building exercises from `{bank_path}`")
+    print(f"Building exercises from `{bank_path}`")
 
-b = Bank(bank_path)
-b.generate_exercises(regenerate=args.full, images=args.full)
-b.write_json()
+    b = Bank(bank_path)
+    b.generate_exercises(regenerate=full, images=full)
+    b.write_json()
 
-# b.build_viewer() # <-- broken, reimplemented below
-docs_path = os.path.join(b.abspath(), "docs")
-if os.path.exists(docs_path) and os.path.isdir(docs_path):
-    shutil.rmtree(docs_path)
-os.makedirs(docs_path)
-archive = zipfile.ZipFile(static.open_resource("viewer.zip"))
-archive.extractall(docs_path)
-# copy assets
-shutil.copytree(b.build_path(), os.path.join(docs_path,"assets"), dirs_exist_ok=True)
+    # b.build_viewer() # <-- broken, reimplemented below
+    docs_path = os.path.join(b.abspath(), "docs")
+    if os.path.exists(docs_path) and os.path.isdir(docs_path):
+        shutil.rmtree(docs_path)
+    os.makedirs(docs_path)
+    archive = zipfile.ZipFile(static.open_resource("viewer.zip"))
+    archive.extractall(docs_path)
+    # copy assets
+    shutil.copytree(b.build_path(), os.path.join(docs_path,"assets"), dirs_exist_ok=True)
 
-stage_path = os.path.join("output", "stage", args.book, "preview", "exercises")
-print(f"Staging bank at `{stage_path}`")
-# stage bank
-shutil.copytree(
-    docs_path, 
-    stage_path, 
-    dirs_exist_ok=True
-)
+    stage_path = os.path.join("output", "stage", book, "preview", "exercises")
+    print(f"Staging bank at `{stage_path}`")
+    # stage bank
+    shutil.copytree(
+        docs_path, 
+        stage_path, 
+        dirs_exist_ok=True
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Build a CheckIt bank.')
+    parser.add_argument('book', choices=['precalculus', 'calculus', 'linear-algebra'])
+    parser.add_argument('-f', '--full',
+                        action='store_true')
+    args = parser.parse_args()
+    main(book=args.book, full=args.full)
