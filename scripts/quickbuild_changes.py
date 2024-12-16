84d732cecbae53a04417e57d3a1611e975f3be41
Steven Clontz <steven.clontz@gmail.com>
2024-12-16 20:14:54 +0000

    specify origin/main

--- scripts/quickbuild_changes.py
+++ scripts/quickbuild_changes.py
@@ -6,7 +6,7 @@ from lxml import etree
 
 repo = Repo(odbt=GitDB) # https://github.com/gitpython-developers/GitPython/discussions/1180#discussioncomment-408868
 commit_head = repo.commit("HEAD")
-commit_origin_main = repo.commit("main")
+commit_origin_main = repo.commit("origin/main")
 commit_merge_base = repo.merge_base(commit_head, commit_origin_main)[0]
 diff_index = commit_merge_base.diff(commit_head)
 changed_files = [Path(item.a_path) for item in diff_index]
