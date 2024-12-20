c9091507a49cf57470b36e37f9e965668609cbbf
Steven Clontz <steven.clontz@gmail.com>
2024-12-20 18:41:42 +0000

    only subtree build e.g. chapters and sections

--- scripts/quickbuild_changes.py
+++ scripts/quickbuild_changes.py
@@ -19,26 +19,24 @@ def main():
     BOOKS = ['precalculus', 'calculus', 'linear-algebra']
 
     # for each .ptx file, try to build subtree of document
-    for f in [f for f in changed_files if f.suffix == ".ptx"]:
-        book = None
-        for b in BOOKS:
-            b_path = Path("source") / b
-            if b_path in f.parents:
-                book = b
-        if book is not None:
-            t = p.get_target(f"{book}-web-instructor")
-            root = etree.parse(f).getroot()
-            xml_id = root.get(r"{http://www.w3.org/XML/1998/namespace}id")
-            print(f"Building book `{book}` with ID `{xml_id}`")
-            title_ele = root.find("title")
-            if title_ele is None:
-                if xml_id is None:
-                    path = f"/preview/{book}/instructor"
-                else:
-                    path = f"/preview/{book}/instructor/{xml_id}.html"
-            else:
+    for b in BOOKS:
+        PTX_FILES = [f for f in changed_files if Path("source", b) in f.parents and f.suffix == ".ptx"]:
+        xml_ids = set()
+        root = etree.parse(f).getroot()
+        if root.tag not in ["section", "chapter", "preface", "appendix", "frontmatter"]:
+            xml_ids.add(None)
+        else:
+            xml_ids.add(root.get(r"{http://www.w3.org/XML/1998/namespace}id"))
+        t = p.get_target(f"{b}-web-instructor")
+        for xml_id in xml_ids:
+            if xml_id is not None:
+                print(f"Building book `{b}` with ID `{xml_id}`")
                 path = f"/preview/{book}/instructor/{xml_id}.html"
-            t.build(xmlid=xml_id, no_knowls=True, generate=(xml_id is not None))
+                t.build(xmlid=xml_id, no_knowls=True, generate=True)
+            else:
+                print(f"Building book `{b}`, skipping images")
+                path = f"/preview/{book}/instructor"
+                t.build(xmlid=None, no_knowls=True, generate=False)
             preview_links.append({
                 "file": f,
                 "path": path
