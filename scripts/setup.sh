1fd6b74a3c3085ca2c109400f8fa8d611bb62484
Steven Clontz <steven.clontz@gmail.com>
2025-04-17 20:39:16 +0000

    update codespace config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,19 @@
+sudo apt-get update 
+sudo apt-get install -y --no-install-recommends \
+                    python3-louis \
+                    libcairo2-dev \
+                    librsvg2-bin
+
 pip install -r requirements.txt
 git config core.hooksPath .githooks
 touch codechat_config.yaml
 pretext --version
+
+playwright install-deps
+
+playwright install
+
+# # Install mermaid for diagrams
+# npm install -g @mermaid-js/mermaid-cli
+
+# prefig init
