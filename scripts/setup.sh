0e44707b38f01fa113f5392ccf2f52a98ac48cdc
Steven Clontz <steven.clontz@gmail.com>
2025-04-18 09:56:00 -0500

    update codespace config (#723)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,27 @@
-pip install -r requirements.txt
+sudo apt-get update 
+sudo apt-get install -y --no-install-recommends \
+                    python3-louis \
+                    libcairo2-dev \
+                    librsvg2-bin
+
+pip install -r requirements.txt # install on default python
+
+bash ./.devcontainer/installSage.sh
+bash ./.devcontainer/installLatex.sh
+tlmgr install tikz-cd
+git config --global --add safe.directory $(pwd)
+
+eval "$('conda' 'shell.bash' 'hook' 2> /dev/null)"
+conda activate sage
+python -m pip install -r requirements.txt # install on conda python
 git config core.hooksPath .githooks
 touch codechat_config.yaml
-pretext --version
+
+playwright install-deps
+
+playwright install
+
+# # Install mermaid for diagrams
+# npm install -g @mermaid-js/mermaid-cli
+
+# prefig init
